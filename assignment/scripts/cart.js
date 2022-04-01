d6b41b7b2f96d6d6e2b0f361191bcfff9f3b450c
Frank Raso <frankraso91@gmail.com>
2022-04-01 13:38:00 -0400

    q1

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,12 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket =[];
+function addItem(string,array){
+array.push(string);
+if(array.includes(string)){
+    return true;
+}
+}
+console.log(addItem(`Hello Dorian`,basket));
+console.log(basket);
